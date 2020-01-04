5577cf830b924331d7188466c13ea64464ef5e95
Karthik Karuppasamy <karthik.k.samy@gmail.com>
2020-01-03 23:04:07 -0800

    Verified image upload to S3 bucket at a custom path. TODO: separate the configs

--- app/app.js
+++ app/app.js
@@ -5,9 +5,57 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var debug = require('debug')('app');
 
+const multer = require('multer');
+//for local image upload
+const Storage = multer.diskStorage({
+  destination(req, file, callback) {
+//    callback(null, './images')
+    callback(null, '/tmp')
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
+  },
+})
+
+const upload = multer({ storage: Storage });
+
+
+//AWS Image upload
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+//***************** TODO: Separate the configs
+const ID = 'AKIAWKPZ3EOXLMF4VK55';
+const SECRET = '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';
+const REGION = 'us-west-2';
+const BUCKET_NAME = 'meditation-metrics-kkyp-20200103';
+//'arn:aws:s3:::meditation-metrics-kkyp-20200103';
+
+aws.config.update({
+    secretAccessKey: SECRET,
+    accessKeyId: ID,
+    region: REGION
+});
+
+var s3 = new aws.S3();
+var StorageS3 = multerS3({
+        s3: s3,
+        bucket: function (req, file, cb) {
+            console.log(`${BUCKET_NAME}/${file.fieldname}`);
+            cb(null, `${BUCKET_NAME}/${file.fieldname}`); 
+        },
+        //bucket: BUCKET_NAME,
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`); 
+        }
+    });
+var uploadS3 = multer({ storage: StorageS3 });
+
+
 const todoRouter = require('./routes/todoRoutes');
 
 var app = express();
+
 console.log('this is the __dirname %s',__dirname);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,6 +86,24 @@ app.post('/task/delete/:id', todoRouter);
 app.get('/task/complete/:id', todoRouter);
 app.get('/task/add/', todoRouter);
 app.post('/task/add/', todoRouter);
+
+//for image upload
+app.post('/api/uploads3', uploadS3.array('photo', 3), (req, res) => {
+  console.log('file', req.files)
+  console.log('body', req.body)
+  res.status(200).json({
+    message: 'success!',
+  })
+})
+
+//for image upload
+app.post('/api/upload', upload.array('photo', 3), (req, res) => {
+  console.log('file', req.files)
+  console.log('body', req.body)
+  res.status(200).json({
+    message: 'success!',
+  })
+})
 
 // catch favicon requests and respond
 app.use('/favicon.ico', (req, res) => res.status(204));
